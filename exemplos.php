57fa1f7af23a2fff26a2bbbfbf8c38db48b0520f
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2018-07-13 01:48:06 -0300

    Ajustes

--- exemplos.php
+++ exemplos.php
@@ -1,3 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  	<div class="container">
+    <h1>Hello, world!</h1>
+
+
+
 <?php
 #Importação da classe
 require "rnh.class.php";
@@ -44,4 +62,46 @@ Observações:
 */
 
 echo '<br>';
+?>
 
+<form action="" method="post" enctype="multipart/form-data">
+	<input type="file" multiple="multiple" name="arquivo_name[]" id="arquivo_id"/>
+	
+	<button class="btn btn-default" type="submit">Enviar</button>
+</form>
+
+<?php 
+
+if($_FILES):	
+
+	$files_accept     = array('.jpg','.jpeg', '.gif','.png');
+	$ret = RNH::uploadFiles($_FILES['arquivo_name'],$files_accept,'image');
+	print_r($ret);
+	//echo $ret;
+
+
+
+	echo '<pre> Arquivos enviados<br>';
+	print_r($_FILES);
+
+endif;
+
+
+//Verificação de Segurança
+$url = $_SERVER["REQUEST_URI"];
+
+if(preg_match("/exemplos.php/", $url)){
+
+	echo 'achou';
+}
+
+?>
+
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>
--- rnh.class.php
+++ rnh.class.php
@@ -1,4 +1,5 @@
 <?php
+
 class RNH{
 
 	public static function dateFormat($date,$format){		
@@ -33,6 +34,115 @@ class RNH{
 		return $valor;
 	}#Fim do método getExt
 
+
+	public static function uploadFile($file,$files_accept,$type_file){
+
+		$return = array();
+
+	  	$pasta    = "../uploads/"; //nome da pasta que ira salvar os arquivos
+	  	if(!file_exists($pasta)){mkdir($pasta, 0755);}//se a pasta não existiir cria a pasta
+
+	  	if($file['tmp_name']){
+
+	    $extencao   = self::getExt($file['name']);//pega a extenção do arquivo
+	    $filename   = md5(time()).$extencao;//nome unico para o arquivo, exitar conflito
+
+	    if(in_array($extencao, $files_accept)){
+
+	    	$pasta  = $pasta.$type_file.'/';
+	      
+	    if(!file_exists($pasta)){mkdir($pasta, 0755);}
+
+	    if(move_uploaded_file($file['tmp_name'], $pasta.$filename)){
+	        
+	        
+	        $return['error_number']   = 0;
+	        $return['error_detail']  = "Sucesso!";
+	        
+	      
+	    }else{
+	        
+	        
+	        $return['error_number']   = 1;
+	        $return['error_detail']  = "Não foi possível realizar esta operação";
+	        
+	        
+	    }//fim da area que move o arquivo
+
+
+	    }else{      
+
+	        
+	        $return['error_number']   	= 1;
+	        $return['error_detail']  	= "O arquivo selecionado não é aceito";       
+	       
+	    }
+
+
+	  	}else{
+
+	        $return['error_number']   = 1;
+	    	$return['error_detail']  = "Nenhum arquivo selecionado";   
+	    
+		}
+
+
+		return $return;
+	}#fim do método de upload de arquivos
+
+
+
+	public static function uploadFiles($file,$files_accept,$type_file){
+		$total_files = count($file['name']);
+		
+		for($x=0;$x<$total_files;$x++){
+			
+
+		$pasta    = "../uploads/"; //nome da pasta que ira salvar os arquivos
+	  	if(!file_exists($pasta)){mkdir($pasta, 0755);}//se a pasta não existiir cria a pasta
+
+	  
+
+	    $extencao   = self::getExt($file['name'][$x]);//pega a extenção do arquivo
+	    $filename   = md5(time()).$extencao;//nome unico para o arquivo, exitar conflito
+
+	    if(in_array($extencao, $files_accept)){
+
+	    	$pasta  = $pasta.$type_file.'/';
+	      
+	    if(!file_exists($pasta)){mkdir($pasta, 0755);}
+
+	    if(move_uploaded_file($file['tmp_name'][$x], $pasta.$filename)){
+	        
+	        
+	        $return['error_number']   = 0;
+	        $return['error_detail']  = "Sucesso!";
+	        
+	      
+	    }else{
+	        
+	        
+	        $return['error_number']   = 1;
+	        $return['error_detail']  = "Não foi possível realizar esta operação";
+	        
+	        
+	    }//fim da area que move o arquivo
+
+
+	    }else{      
+
+	        
+	        $return['error_number']   	= 1;
+	        $return['error_detail']  	= "O arquivo selecionado não é aceito";       
+	       
+	    }
+
+
+		}#EndFor
+
+		return $return;
+
+	}#fim do método de upload de arquivos mutiplos
 
 
 }# Fim da classe
